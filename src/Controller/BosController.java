dfa82ad4a6aa6b7fabf71b5f9dc14eaa98cc6706
calvi <calvi@LAPTOP-7Q8TNS4V.mshome.net>
2022-07-08 18:52:46 +0700

    aku commit controller banyak

---
+++ src/Controller/BosController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author calvi
+ */
+import Model.*;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class BosController {
+    
+    ConnectDatabase conn = new ConnectDatabase();
+    public ArrayList<Double> viewPendapatanTotal(){
+        ArrayList<Double> listTotalPerCabang = new ArrayList<>();
+        int i = 1;
+        CabangController cabang = new CabangController();
+        int jumlahCabang = cabang.countCabang();
+        while(i<jumlahCabang){
+            String query = "SELECT totalBayar FROM transaksi WHERE idCabang="+i;
+            conn.connect();
+            try{
+                Statement stmt = conn.con.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                double total = 0;
+                while(rs.next()){
+                    total += rs.getDouble("totalBayar");
+                }
+                listTotalPerCabang.add(total);
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
+        conn.disconnect();
+        return listTotalPerCabang;
+    }
+    public String addCabang(){
+        return "";
+    }
+}
